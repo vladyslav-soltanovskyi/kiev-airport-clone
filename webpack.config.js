632f6fcc1df12dd4a3ab264e37e60103153a609a
vladyslav-soltanovskyi <vladsoltanovskih@gmail.com>
2023-01-27 19:03:21 +0100

    init project

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  const config = {
+    entry: './src/index.tsx',
+    output: {
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
+          }
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.tsx', '.ts'],
+      alias: {
+        'components': path.resolve(__dirname, '.', 'src', 'components'),
+      },
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      open: true,
+      hot: true,
+      port: 8080,
+    },
+  };
+
+  if (isProduction) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
+
+  return config;
+};
